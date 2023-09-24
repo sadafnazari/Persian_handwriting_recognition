4b791a2e4f5ccc824c7556aa4f00de95086c24f1
sadaf.nazari <sadaf.nazari@gimrobotics.fi>
2023-09-24 15:16:12 +0300

    extract from form refactored

--- test/extract_data_from_form.py
+++ test/extract_data_from_form.py
@@ -1,8 +1,9 @@
-import numpy as np
-import cv2
+import glob
 import math
 import os
-import glob
+
+import cv2
+import numpy as np
 import yaml
 
 
@@ -11,42 +12,53 @@ def aruco_extraction(img):
     Extracts the aruco signs from the given image, and selects the boundaries points of the form
 
     Args:
-        img (numpy.ndarray): an image from the test forms
+        img (numpy.ndarray): an image
 
     Returns:
         numpy.ndarray or None: boundaries of the form
     """
-    # Load the dictionary that was used to generate the markers.
     dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+
     # Initialize the detector parameters using default values
     parameters = cv2.aruco.DetectorParameters_create()
+
     # Detect the markers in the image
-    marker_corners, marker_ids, rejected_candidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
-    # print("{} ArUco marker were detected.".format(len(marker_corners)))
-    assert len(marker_corners) == 4, "You have detected {} instead of 4 ArUco markers!".format(len(marker_corners))
-    p1 = (0, 0)
-    p2 = (0, 0)
-    p3 = (0, 0)
-    p4 = (0, 0)
-    # set the corners based on their ids
-    for m in range(len(marker_ids)):
-        if marker_ids[m] == 34:
-            p1 = marker_corners[m][0][3][0], marker_corners[m][0][3][1]
-        elif marker_ids[m] == 36:
-            p2 = marker_corners[m][0][1][0], marker_corners[m][0][1][1]
-        elif marker_ids[m] == 35:
-            p3 = marker_corners[m][0][2][0], marker_corners[m][0][2][1]
-        else:
-            p4 = marker_corners[m][0][0][0], marker_corners[m][0][0][1]
-    # print(p1, p2, p3, p4)
-    return p1, p2, p3, p4
+    marker_corners, marker_ids, rejected_candidates = cv2.aruco.detectMarkers(
+        img, dictionary, parameters=parameters
+    )
+
+    # Checks how many markers are detected
+    if len(marker_corners) != 4:
+        print("{} arucos detected instead of 4!".format(len(marker_corners)))
+        return None
+
+    # flatten the marker_corners array
+    marker_corners = [mc[0] for mc in marker_corners]
+
+    # corners based on the ids [34: top left, 35:top right, 36:bottom right, 33:bottom left]
+
+    # selects the boundaries clock wise(top left point of the top left marker,
+    #                                   top right point of the top right marker,
+    #                                   bottom right point of the bottom right marker,
+    #                                   bottom left point of the bottom left marker)
+    boundaries = np.array(
+        [
+            marker_corners[2][3],
+            marker_corners[1][2],
+            marker_corners[0][1],
+            marker_corners[3][0],
+        ],
+        dtype=np.float32,
+    )
+    return boundaries
+
 
 def form_extraction(img, corners, form_width, form_height):
     """
     Applies perspective to the image and extracts the form
 
     Args:
-        img (numpy.ndarray): an image from the test forms
+        img (numpy.ndarray): an image
         corners (numpy.ndarray): position of the corners of the form
         form_width (int): width of the form
         form_height (int): height of the form
@@ -54,41 +66,20 @@ def form_extraction(img, corners, form_width, form_height):
     Returns:
         numpy.ndarray: image of the extracted form
     """
-    # extract the form to a 800 * 800 picture and correct its perspective
-    print(corners)
-    points1 = corners
-    n = 800
-    m = 800
-    J = np.zeros((m, n))
-    cv2.imshow('Original image', img)
-    cv2.waitKey(0)
-    points2 = np.array([(0, 0),
-                        (n, 0),
-                        (n, m),
-                        (0, m)]).astype(np.float32)
-    print(points1)
-    print(points2)
-    print(points1.dtype, points2.dtype)
-    #H = np.array([[ 1.03459088e+00,  5.70022525e-03, -3.72709228e+01],
-    #              [-3.10793092e-02,  1.08925579e+00, -2.01513801e+02],
-    #             [-5.94946334e-05, -1.04173685e-05,  1.00000000e+00]])
-    H = cv2.getPerspectiveTransform(points1, points2)
-    output_size = (J.shape[1], J.shape[0])
-    print(output_size, H)
-    J = cv2.warpPerspective(img, H, output_size)
-    cv2.imshow('Form', J)
-    print("1234")
-    cv2.waitKey(0)
-    return J
+    form_points = np.array(
+        [(0, 0), (form_width, 0), (form_width, form_height), (0, form_height)]
+    ).astype(np.float32)
+
+    # applies perspective tranformation
+    perspective_transformation = cv2.getPerspectiveTransform(corners, form_points)
+    form = cv2.warpPerspective(
+        img, perspective_transformation, (form_width, form_height)
+    )
+    return form
+
 
 def cell_extraction(
-    img,
-    img_path,
-    extracted_path,
-    form_width,
-    form_height,
-    cell_width,
-    cell_height,
+    img, img_path, extracted_path, form_width, form_height, cell_width, cell_height
 ):
     """
     Extracts cells and the saves them based on the type of the given form
@@ -102,169 +93,99 @@ def cell_extraction(
         cell_width (int): width of each cell
         cell_height (int): height of each cell
     """
-    # Calculate cell dimensions based on the number of lines
-        # extract each letter, number and checkbox
-    return
-    I = img
-    G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
-    # first extracting edges
-    E = cv2.Canny(G, 50, 120)
-    # then find the corners by Harris
-    G = np.float32(E)
-    window_size = 4
-    soble_kernel_size = 5
-    alpha = 0.04
-    H = cv2.cornerHarris(G, window_size, soble_kernel_size, alpha)
-    H = H / H.max()
-    M = np.uint8(H > 0.005) * 255
+    image_name = os.path.basename(img_path)
+    directory_name = os.path.splitext(image_name)[0]
+    directory = extracted_path + "/" + directory_name
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Folder '{directory}' created.")
+    else:
+        print(f"Folder '{directory}' already exists.")
+    # Locations are guessed TODO: Find a proper way to apply this
+    starting_points = {"ID": (40, 305), "First Name": (40, 435), "Last Name": (40, 570)}
+    ending_points = {
+        "ID": (580, 385),
+        "First Name": (580, 515),
+        "Last Name": (580, 650),
+    }
+    degree_points = {"PHD": (75, 720), "MS": (225, 720), "BS": (450, 720)}
 
-    # guess the location of the principal points
-    starting_points = {'ID': (40, 200), 'First Name': (40, 310), 'Last Name': (40, 400)}
-    ending_points = {'ID': (585, 280), 'First Name': (585, 372), 'Last Name': (585, 470)}
-    degree_points = {'PHD': (75, 510), 'MS': (220, 510), 'BS': (450, 510)}
+    number_of_cells = 8
 
-    # # extracting!
-    # # Student ID
-    # starting_flag = True
-    # ending_flag = True
-    # for i in range(800):
-    #     if starting_flag or ending_flag:
-    #         for j in range(800):
-    #             if starting_flag or ending_flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(starting_points['ID'], (j, i)) <= 13.0 and starting_flag:
-    #                         starting_points['ID'] = (j, i)
-    #                         # cv2.circle(img, (starting_points['ID']), 5, (0, 0, 255), -1)
-    #                         starting_flag = False
-    #                     if calculate_distance(ending_points['ID'], (j, i)) <= 13.0 and ending_flag:
-    #                         ending_points['ID'] = (j, i)
-    #                         # cv2.circle(img, (ending_points['ID']), 5, (0, 0, 255), -1)
-    #                         ending_flag = False
+    for i in range(number_of_cells):
+        width = (ending_points["ID"][0] - starting_points["ID"][0]) // 8
+        x1 = i * width + starting_points["ID"][0]
+        y1 = starting_points["ID"][1]
+        x2 = x1 + width
+        y2 = ending_points["ID"][1]
+        cell = img[y1:y2, x1:x2]
+        cell = cv2.resize(cell, (cell_width, cell_height))
+        cv2.imwrite(directory + "/" + "ID" + "_" + str(i) + ".jpg", cell)
 
-    # # Student First Name
-    # starting_flag = True
-    # ending_flag = True
-    # for i in range(800):
-    #     if starting_flag or ending_flag:
-    #         for j in range(800):
-    #             if starting_flag or ending_flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(starting_points['First Name'], (j, i)) <= 13.0 and starting_flag:
-    #                         starting_points['First Name'] = (j, i)
-    #                         # cv2.circle(img, (starting_points['First Name']), 5, (0, 0, 255), -1)
-    #                         starting_flag = False
-    #                     if calculate_distance(ending_points['First Name'], (j, i)) <= 13.0 and ending_flag:
-    #                         ending_points['First Name'] = (j, i)
-    #                         # cv2.circle(img, (ending_points['First Name']), 5, (0, 0, 255), -1)
-    #                         ending_flag = False
+    for i in range(number_of_cells):
+        width = (ending_points["First Name"][0] - starting_points["First Name"][0]) // 8
+        x1 = i * width + starting_points["First Name"][0]
+        y1 = starting_points["First Name"][1]
+        x2 = x1 + width
+        y2 = ending_points["First Name"][1]
+        cell = img[y1:y2, x1:x2]
+        cell = cv2.resize(cell, (cell_width, cell_height))
+        cv2.imwrite(directory + "/" + "FN" + "_" + str(i) + ".jpg", cell)
 
-    # # Student Last Name
-    # starting_flag = True
-    # ending_flag = True
-    # for i in range(800):
-    #     if starting_flag or ending_flag:
-    #         for j in range(800):
-    #             if starting_flag or ending_flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(starting_points['Last Name'], (j, i)) <= 13.0 and starting_flag:
-    #                         starting_points['Last Name'] = (j, i)
-    #                         # cv2.circle(img, (starting_points['Last Name']), 5, (0, 0, 255), -1)
-    #                         starting_flag = False
-    #                     if calculate_distance(ending_points['Last Name'], (j, i)) <= 13.0 and ending_flag :
-    #                         ending_points['Last Name'] = (j, i)
-    #                         # cv2.circle(img, (ending_points['Last Name']), 5, (0, 0, 255), -1)
-    #                         ending_flag = False
+    for i in range(number_of_cells):
+        width = (ending_points["Last Name"][0] - starting_points["Last Name"][0]) // 8
+        x1 = i * width + starting_points["Last Name"][0]
+        y1 = starting_points["Last Name"][1]
+        x2 = x1 + width
+        y2 = ending_points["Last Name"][1]
+        cell = img[y1:y2, x1:x2]
+        cell = cv2.resize(cell, (cell_width, cell_height))
+        cv2.imwrite(directory + "/" + "LN" + "_" + str(i) + ".jpg", cell)
 
-    # # Student Degree BS
-    # point = degree_points['BS']
-    # flag = True
-    # for i in range(800):
-    #     if flag:
-    #         for j in range(800):
-    #             if flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(point, (j, i)) <= 10.0:
-    #                         # cv2.circle(img, (j, i), 5, (255, 0, 0), -1)
-    #                         degree_points['BS'] = (j, i)
-    #                         flag = False
-    # # Student Degree MS
-    # point = degree_points['MS']
-    # flag = True
-    # for i in range(800):
-    #     if flag:
-    #         for j in range(800):
-    #             if flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(point, (j, i)) <= 15.0:
-    #                         # cv2.circle(img, (j, i), 5, (255, 0, 0), -1)
-    #                         degree_points['MS'] = (j, i)
-    #                         flag = False
-    # # Student Degree PHD
-    # point = degree_points['PHD']
-    # flag = True
-    # for i in range(800):
-    #     if flag:
-    #         for j in range(800):
-    #             if flag:
-    #                 if M[i][j] == 255:
-    #                     if calculate_distance(point, (j, i)) <= 15.0:
-    #                         # cv2.circle(img, (j, i), 5, (255, 0, 0), -1)
-    #                         degree_points['PHD'] = (j, i)
-    #                         flag = False
+    x1_bs = degree_points["BS"][0]
+    y1_bs = degree_points["BS"][1]
+    x2_bs = degree_points["BS"][0] + 25
+    y2_bs = degree_points["BS"][1] + 25
+    cell = img[y1_bs:y2_bs, x1_bs:x2_bs]
+    cell = cv2.resize(cell, (cell_width, cell_height))
+    cv2.imwrite(directory + "/" + "BS" + ".jpg", cell)
+
+    x1_ms = degree_points["MS"][0]
+    y1_ms = degree_points["MS"][1]
+    x2_ms = degree_points["MS"][0] + 25
+    y2_ms = degree_points["MS"][1] + 25
+    cell = img[y1_ms:y2_ms, x1_ms:x2_ms]
+    cell = cv2.resize(cell, (cell_width, cell_height))
+    cv2.imwrite(directory + "/" + "MS" + ".jpg", cell)
+
+    x1_phd = degree_points["PHD"][0]
+    y1_phd = degree_points["PHD"][1]
+    x2_phd = degree_points["PHD"][0] + 25
+    y2_phd = degree_points["PHD"][1] + 25
+    cell = img[y1_phd:y2_phd, x1_phd:x2_phd]
+    cell = cv2.resize(cell, (cell_width, cell_height))
+    cv2.imwrite(directory + "/" + "PHD" + ".jpg", cell)
 
 
-    #     for i in range(8):
-    #     w = ending_points['ID'][0] - starting_points['ID'][0]-10
-    #     h = ending_points['ID'][1] - starting_points['ID'][1]-10
-    #     x = starting_points['ID'][0]+5
-    #     y = starting_points['ID'][1]+5
-    #     cell = img[y:y+h, math.floor(x+((i)*(w/8))):math.floor(x+((i+1)*(w/8)))]
-    #     cv2.imwrite(path + "/" + "ID" + str(i) + ".jpg", cell)
+def extract_and_save(
+    test_form_path, extracted_path, form_width, form_height, cell_width, cell_height
+):
+    """
+    Extracts forms from the images and then extracts the cells and saves the results.
 
-    # # Student First Name extraction
-    # for i in range(8):
-    #     w = ending_points['First Name'][0] - starting_points['First Name'][0]-10
-    #     h = ending_points['First Name'][1] - starting_points['First Name'][1]-10
-    #     x = starting_points['First Name'][0]+5
-    #     y = starting_points['First Name'][1]+5
-    #     cell = img[y:y+h, math.floor(x+((i)*(w/8))):math.floor(x+((i+1)*(w/8)))]
-    #     cv2.imwrite(path + "/" + "FN" + str(7-i) + ".jpg", cell)
-
-    # # Student Last Name extraction
-    # for i in range(8):
-    #     w = ending_points['Last Name'][0] - starting_points['Last Name'][0]-10
-    #     h = ending_points['Last Name'][1] - starting_points['Last Name'][1]-10
-    #     x = starting_points['Last Name'][0]+5
-    #     y = starting_points['Last Name'][1]+5
-    #     cell = img[y:y+h, math.floor(x+((i)*(w/8))):math.floor(x+((i+1)*(w/8)))]
-    #     cv2.imwrite(path + "/" + "LN" + str(7-i) + ".jpg", cell)
-
-    # # BS extraction
-    # cell = img[degree_points['BS'][1] : degree_points['BS'][1] + 25, degree_points['BS'][0] : degree_points['BS'][0] + 25]
-    # cv2.imwrite(path + "/" + "BS.jpg", cell)
-
-    # # MS extraction
-    # cell = img[degree_points['MS'][1] : degree_points['MS'][1] + 25, degree_points['MS'][0] : degree_points['MS'][0] + 25]
-    # cv2.imwrite(path + "/" + "MS.jpg", cell)
-
-    # # PHD extraction
-    # cell = img[degree_points['PHD'][1] : degree_points['PHD'][1] + 25, degree_points['PHD'][0] : degree_points['PHD'][0] + 25]
-    # cv2.imwrite(path + "/" + "PHD.jpg", cell)
-
-
-def calculate_distance(p1, p2):
-    dist = math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-    return dist
-
-
-def extract_and_save(test_form_path, extracted_path, form_width, form_height, cell_width, cell_height):
+    Args:
+        test_form_path (str): path of the test form
+        extracted_path (str): path for saving the extracted cells
+        form_width (int): width of the form
+        form_height (int): height of the form
+        cell_width (int): width of each cell
+        cell_height (int): height of each cell
+    """
     for image_path in glob.glob(test_form_path + "/*.*"):
-        image = cv2.imread('data/test_form/forms/test.jpg')
-        corners = np.array(aruco_extraction(image), dtype=np.float32)
+        image = cv2.imread(image_path)
+        corners = aruco_extraction(image)
         if corners is None:
-            print(
-                f"The image {image_path[image_path.find('/' + type + '/')+3:]} is dropped."
-            )
+            print(f"The image {image_path} is dropped.")
             continue
         form = form_extraction(image, corners, form_width, form_height)
         cell_extraction(
@@ -276,6 +197,7 @@ def extract_and_save(test_form_path, extracted_path, form_width, form_height, ce
             cell_width,
             cell_height,
         )
+
 
 def check_config_file(config_path):
     """
@@ -334,18 +256,18 @@ def check_config_keys(cfg, required_keys):
 def data_extraction(config):
 
     required_keys = [
-        "test_form.test_form_path",
-        "test_form.extracted_path",
+        "test_forms.test_form_path",
+        "test_forms.extracted_path",
         "pre_processing.form_width",
         "pre_processing.form_height",
         "pre_processing.cell_width",
-        "pre_processing.cell_height"
+        "pre_processing.cell_height",
     ]
 
     check_config_keys(config, required_keys)
 
-    test_form_path = config["test_form"].get("test_form_path")
-    extracted_path = config["test_form"].get("extracted_path")
+    test_form_path = config["test_forms"].get("test_form_path")
+    extracted_path = config["test_forms"].get("extracted_path")
 
     form_width = config["pre_processing"].get("form_width")
     form_height = config["pre_processing"].get("form_height")
@@ -359,10 +281,12 @@ def data_extraction(config):
     else:
         print(f"Folder '{extracted_path}' already exists.")
 
-    extract_and_save(test_form_path, extracted_path, form_width, form_height, cell_width, cell_height)
+    extract_and_save(
+        test_form_path, extracted_path, form_width, form_height, cell_width, cell_height
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config_path = "config/config.yaml"
     config = check_config_file(config_path)
     data_extraction(config)
